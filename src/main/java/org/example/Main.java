c7558f111b0611ddbec371195a2bf225f7ac51bd
Nazarova_Vladislava <nvv.03@yandex.ru>
2024-09-18 10:13:28 +0700

    красивые логи

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,10 +5,10 @@ import java.util.Scanner;
 import org.json.JSONObject;
 import java.io.PrintWriter;
 import java.io.FileWriter;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 //D:\Development-of-integration-systems\lab_1_1\1.txt
-
-// дата и время в лог файле!!
 
 public class Main {
     public static void main(String[] args) {
@@ -28,8 +28,12 @@ public class Main {
                 int choice = scanner.nextInt();
                 scanner.nextLine();  // Потребляется оставшийся перевод строки
 
+                // Получаем текущие дату и время
+                LocalDateTime now = LocalDateTime.now();
+                DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
                 // Записываем выбор пользователя в лог
-                logWriter.println("Пользователь выбрал действие: " + choice);
+                logWriter.println();
+                logWriter.println(now.format(formatter) + " Пользователь выбрал действие: " + choice);
 
                 // Обработка выбора пользователя
                 if (choice == 1) {
@@ -38,22 +42,22 @@ public class Main {
                     String url = scanner.nextLine();
 
                     // Логирование URL
-                    logWriter.println("Пользователь ввел URL: " + url);
+                    logWriter.println(now.format(formatter) + " Пользователь ввел URL: " + url);
 
                     try {
                         System.out.println("Программа получает данные по указанному URL...");
-                        logWriter.println("Программа получает данные по URL: " + url);
+                        logWriter.println(now.format(formatter) + " Программа получает данные по URL: " + url);
 
                         JSONObject json = jsonReader.readJsonFromUrl(url);
                         System.out.println("Данные получены успешно!");
                         System.out.println(json.toString(2));
 
-                        logWriter.println("Данные успешно получены");
+                        logWriter.println(now.format(formatter) + " Данные успешно получены");
 
                     } catch (IOException e) {
                         // Логирование ошибки
                         System.err.println("Ошибка при получении данных: " + e.getMessage());
-                        logWriter.println("Ошибка при получении данных: " + e.getMessage());
+                        logWriter.println(now.format(formatter) + " Ошибка при получении данных: " + e.getMessage());
                     }
 
                 } else if (choice == 2) {
@@ -62,34 +66,34 @@ public class Main {
                     String filePath = scanner.nextLine();
 
                     // Логирование пути к файлу
-                    logWriter.println("Пользователь ввел путь к файлу: " + filePath);
+                    logWriter.println(now.format(formatter) + " Пользователь ввел путь к файлу: " + filePath);
 
                     try {
                         System.out.println("Программа считывает данные из файла...");
-                        logWriter.println("Программа считывает данные из файла: " + filePath);
+                        logWriter.println(now.format(formatter) + " Программа считывает данные из файла: " + filePath);
 
                         JSONObject json = jsonReader.readJsonFromFile(filePath);
                         System.out.println("Данные получены успешно!");
                         System.out.println(json.toString(2));
 
-                        logWriter.println("Данные успешно получены");
+                        logWriter.println(now.format(formatter) + " Данные успешно получены");
 
                     } catch (IOException e) {
                         // Логирование ошибки
                         System.err.println("Ошибка при получении данных: " + e.getMessage());
-                        logWriter.println("Ошибка при получении данных: " + e.getMessage());
+                        logWriter.println(now.format(formatter) + " Ошибка при получении данных: " + e.getMessage());
                     }
 
                 } else if (choice == 0) {
                     // Пользователь выбрал выход из программы
                     System.out.println("Завершение программы.");
-                    logWriter.println("Пользователь завершил программу.");
+                    logWriter.println(now.format(formatter) + " Пользователь завершил программу.");
                     break;  // Выход из цикла и завершение программы
 
                 } else {
                     // Если введено неверное число
                     System.out.println("Неверный выбор. Попробуйте снова.");
-                    logWriter.println("Неверный выбор пользователя.");
+                    logWriter.println(now.format(formatter) + " Неверный выбор пользователя.");
                 }
             }
 
